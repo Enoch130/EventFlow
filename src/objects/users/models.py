1257585e30fdd855b9e9067f7e0ffc26796279b1
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2025-12-29 13:08:36 +0000

    changes to the models

--- src/objects/users/models.py
+++ src/objects/users/models.py
@@ -4,10 +4,10 @@ from sqlalchemy.dialects.postgresql import UUID as pgUUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 class User(UUIDModel, TimestampedModel):
-    firstname:Mapped[str] = mapped_column(String, nullable=False)
-    middlename:Mapped[str] = mapped_column(String, nullable=True)
-    lastname:Mapped[str] = mapped_column(String, nullable=False)
+    firstName:Mapped[str] = mapped_column(String)
+    middleName:Mapped[str] = mapped_column(String, nullable=True)
+    lastName:Mapped[str] = mapped_column(String)
     email:Mapped[str] = mapped_column(String,unique=True, index = True)
-    phonenumber:Mapped[str] = mapped_column(String,nullable=True)
-    password:Mapped[str] = mapped_column(String,nullable=False)
-    role:Mapped[str] = mapped_column(String, nullable=False) # is there alternative to give options
+    phoneNumber:Mapped[str] = mapped_column(String,nullable=True)
+    password:Mapped[str] = mapped_column(String)
+    role:Mapped[str] = mapped_column(String)
