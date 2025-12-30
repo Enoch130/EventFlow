1e45da92ad03ab7a8d164bfed15b026ddae1ad9e
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2025-12-30 19:29:40 +0000

    done with the vendor model, closes #6

---
+++ src/objects/vendor/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String,Integer
+from src.models.base import UUIDModel, TimestampedModel
+from sqlalchemy.dialects.postgresql import UUID as pgUUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(UUIDModel, TimestampedModel):
+    name:Mapped[str] = mapped_column(String)
+    category:Mapped[str] = mapped_column(String)
+    status:Mapped[str] = mapped_column(String)
+    email:Mapped[str] = mapped_column(String,unique=True, index = True)
+    phoneNumber:Mapped[str] = mapped_column(String,nullable=True)
